607ae7c03917223d8119d12e20657bdd8c21577c
SiwarHaddad <96549904+SiwarHaddad@users.noreply.github.com>
2022-05-09 11:37:22 +0200

    Delete MUSIC_store/View directory

--- MUSIC_store/View/footer.php
+++
@@ -1,9 +0,0 @@
-<!-- Footer -->
-
-<footer class="bg3 p-t-75 p-b-32">
-    <div class="container">
-        <p class="stext-107 cl6 txt-center">
-            Copyright &copy;2022 All rights reserved | Siwar Haddad
-        </p>
-    </div>
-</footer>
